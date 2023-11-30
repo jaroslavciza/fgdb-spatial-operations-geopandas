c79720aa75efbbeafed068cc1a05b1248991df4d
Jaroslav Cíza <jaroslav.ciza@gmail.com>
2023-11-30 22:35:41 +0100

    script init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # geopandas-test
-Geopandas - python library for work with geospatial data (free ArcPy)
+Python Script for converting XLSX with GPS coords (will be updated for SQL query) to file geodatabase and creating a 500m buffer 
+
+It uses Geopandas - python library for work with geospatial data (free ArcPy)
--- script.py
+++ script.py
@@ -1,42 +1,35 @@
-import random
+import pandas as pd
 import geopandas as gpd
-import matplotlib.pyplot as plt
-from shapely.geometry import Point
+#import tkinter as tk
+from tkinter import filedialog
 
-# Seznam s názvy měst v České republice
-cities = ["Praha", "Brno", "Ostrava", "Plzeň", "Liberec", "Olomouc", "České Budějovice", "Hradec Králové", "Ústí nad Labem", "Pardubice",
-          "Zlín", "Jihlava", "Tábor", "Karlovy Vary", "Český Krumlov", "Kutná Hora", "Teplice", "Cheb", "Kladno", "Opava"]
+#import fiona
 
-# Slovník pro ukládání názvu města a jeho náhodných souřadnic
-cities_points = {}
+# vstupní proměnné
+working_dir = "./_data/"
+geodatabase_name = 'output.gdb'
+feature_class_name = 'zpv'
+xlsx_file = ''
 
-# Generování náhodných souřadnic pro každé město
-for city in cities:
-    latitude = round(random.uniform(48.5, 51.5), 6)  # Zeměpisná šířka
-    longitude = round(random.uniform(12.5, 18.5), 6)  # Zeměpisná délka
-    cities_points[city] = (latitude, longitude)
+#xlsx_file = filedialog.askopenfilename(initialdir = working_dir)
 
-# Výpis vytvořeného slovníku
-for city, coords in cities_points.items():
-    print(f"{city}: {coords}")
+if xlsx_file == "":
+    xlsx_file = working_dir + 'zpv.xlsx'
+
+#print (xlsx_file)
+# Načtení XLSX souboru pomocí Pandas
+df = pd.read_excel(xlsx_file)
 
 # Vytvoření GeoDataFrame
-geometry = [Point(lon, lat) for lat, lon in cities_points.values()]
-gdf_cities = gpd.GeoDataFrame(geometry=geometry, index=cities, crs="EPSG:4326")  # WGS84 = 4326
+zpv_gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df['GPS_E'], df['GPS_N']), crs='EPSG:4326')  #WGS84 = 4326
 
-# Stáhnutí mapy světa ze zadaného zdroje
-world_map = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+# Vytvoření bufferu 500 metrů kolem bodů
+buffer_radius = 500  # v metrech
+zpv_gdf_buffer = zpv_gdf.to_crs(epsg=32633)  # Projekce geometrií na vhodný CRS (např. UTM)
+zpv_gdf_buffer['geometry'] = zpv_gdf_buffer['geometry'].buffer(buffer_radius)
 
-# Vytvoření mapy
-ax = world_map.plot(figsize=(10, 6), color='lightgrey')
+# print (fiona.supported_drivers)
 
-# Vykreslení bodů
-gdf_cities.plot(ax=ax, marker='o', color='red', markersize=50)
-
-# Přidání popisků
-for city, (lon, lat) in cities_points.items():
-    plt.text(lon, lat, city, fontsize=8)
-
-# Nastavení titulu a zobrazení mapy
-plt.title('Vizualizace pole bodů v České republice')
-plt.show()
+# Vytvoření souborové geodatabáze a zapsani bodové vrstvy
+zpv_gdf.to_file(f'{working_dir}{geodatabase_name}', driver='OpenFileGDB', layer = "zpv")
+zpv_gdf_buffer.to_file(f'{working_dir}{geodatabase_name}', driver='OpenFileGDB', layer = "zpv_buffer_500m")
